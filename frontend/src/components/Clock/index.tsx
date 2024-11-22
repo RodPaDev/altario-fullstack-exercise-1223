76747c9e052d81babd31c492ef70186c3c645e97
rodpadev <47316946+RodPaDev@users.noreply.github.com>
2024-11-22 12:06:16 +0000

    create basic clock component

---
+++ frontend/src/components/Clock/index.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import Clock from 'react-clock';
+import 'react-clock/dist/Clock.css';
+
+function ClockComponent() {
+    const [time, setTime] = useState<Date>(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => setTime(new Date()), 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return <Clock value={time} size={50} renderMinuteMarks={false} />;
+}
+
+export default ClockComponent;
