9583ceda01a28b20882fb927bd68808b964b0247
rodpadev <47316946+RodPaDev@users.noreply.github.com>
2024-11-22 17:33:40 +0000

    move generation logic to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,84 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useReducer } from 'react';
 import './App.css';
-import GeneratorView, { GridData } from './views/Generator';
+import GeneratorView from './views/Generator';
 import PaymentView from './views/Payment';
 import { getPing } from './api/ping';
+import { generateGrid, getGridCode } from './api/grid';
+import { initialState, reducer, State } from './state';
 
-const PING_INTERVAL_MS = 5000
+const PING_INTERVAL_MS = 5000;
+const GENERATION_STEP_MS = 2000;
 
 function App() {
-  const [gridData, setGridData] = useState<GridData | null>(null);
-  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const pingRef = useRef<number | null>(null);
-
-  const handleGridDataUpdate = (gridData: GridData) => {
-    setGridData(gridData);
-  };
+  const generationIntervalRef = useRef<number | null>(null);
 
   useEffect(() => {
     const checkPingStatus = async () => {
       const ping = await getPing();
-      setIsConnected(ping);
-    }
+      dispatch({ type: 'SET_IS_CONNECTED', payload: ping });
+    };
 
-    checkPingStatus()
+    checkPingStatus();
 
     pingRef.current = setInterval(checkPingStatus, PING_INTERVAL_MS);
 
     return () => {
-      if (pingRef.current) {
-        clearInterval(pingRef.current);
-      }
+      if (pingRef.current) clearInterval(pingRef.current);
     };
-
   }, []);
+
+  const startGridGeneration = async () => {
+    dispatch({ type: 'TOGGLE_GENERATOR' });
+    await fetchGridData();
+
+    generationIntervalRef.current = setInterval(() => {
+      fetchGridData();
+    }, GENERATION_STEP_MS);
+  };
+
+  const stopGridGeneration = () => {
+    dispatch({ type: 'TOGGLE_GENERATOR' });
+    if (generationIntervalRef.current) {
+      clearInterval(generationIntervalRef.current);
+      generationIntervalRef.current = null;
+    }
+  };
+
+  const fetchGridData = async () => {
+    try {
+      const { grid, lastBiasTime, prevBiasChar } = await generateGrid({
+        bias: state.biasChar,
+      });
+      const { code } = await getGridCode();
+
+      if (
+        state.biasChar !== prevBiasChar &&
+        !state.isBiasInputDisabled &&
+        lastBiasTime + 4000 > Date.now()
+      ) {
+        dispatch({ type: 'SET_LAST_BIAS_TIME', payload: lastBiasTime });
+        dispatch({ type: 'SET_BIAS_INPUT_DISABLED', payload: true });
+      }
+
+      dispatch({ type: 'SET_GRID_DATA', payload: { grid, code } });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="main-page">
       <div className="main-content">
         <h1>Altar.io Full-Stack Exercise 1223</h1>
-        <GeneratorView isConnected={isConnected} gridData={gridData} setGridData={handleGridDataUpdate} />
-        <PaymentView gridData={gridData} />
+        <GeneratorView
+          state={state}
+          dispatch={dispatch}
+          startGridGeneration={startGridGeneration}
+          stopGridGeneration={stopGridGeneration}
+        />
+        <PaymentView gridData={state.gridData} />
       </div>
     </div>
   );
--- frontend/src/views/Generator/index.tsx
+++ frontend/src/views/Generator/index.tsx
@@ -1,148 +1,62 @@
-import { useEffect, useRef, useState } from 'react'
-import { generateGrid, getGridCode } from '../../api/grid'
-import clsx from "clsx";
+import Grid from '../../components/Grid';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import ClockComponent from '../../components/Clock';
+import GeneratorStatus from '../../components/GeneratorStatus';
 
-import Grid from '../../components/Grid'
-import Input from '../../components/Input'
-import Button from '../../components/Button'
-import ClockComponent from '../../components/Clock'
+import { State, Action } from '../../state';
 
-import './generator.css'
-
-const GENERATION_STEP_MS = 2000
-const BIAS_COOLDOWN_MS = 4000;
-
-export type GridData = {
-  grid: string
-  code: string
-}
+import './generator.css';
 
 type GeneratorViewProps = {
-  isConnected: boolean,
-  gridData: GridData | null,
-  setGridData: Function
-}
+  state: State;
+  dispatch: React.Dispatch<Action>;
+  startGridGeneration: () => void;
+  stopGridGeneration: () => void;
+};
 
-export default function GeneratorView({ isConnected, gridData, setGridData }: GeneratorViewProps) {
-  const [biasChar, setBiasChar] = useState<string>("")
-  const [isGeneratorStarted, setIsGeneratorStarted] = useState<boolean>(false)
-  const [lastBiasTime, setLastBiasTime] = useState<number | null>(null)
-  const [isBiasInputDisabled, setIsBiasInputDisabled] = useState<boolean>(false)
-
-  let generationIntervalRef = useRef<number | null>(null);
-
-
-  const onHandleBiasChar = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value === "" || /^[a-zA-Z]$/.test(e.target.value)) {
-      setBiasChar(e.target.value)
+export default function GeneratorView({
+  state,
+  dispatch,
+  startGridGeneration,
+  stopGridGeneration,
+}: GeneratorViewProps) {
+  const handleBiasCharChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === '' || /^[a-zA-Z]$/.test(e.target.value)) {
+      dispatch({ type: 'SET_BIAS_CHAR', payload: e.target.value });
     }
-  }
+  };
 
-  useEffect(() => {
-    const startGeneration = async () => {
-      await fetchGridData();
-
-      generationIntervalRef.current = setInterval(() => {
-        fetchGridData();
-      }, GENERATION_STEP_MS);
-    };
-
-    if (isGeneratorStarted) {
-      startGeneration();
+  const handleGenerateGrid = () => {
+    if (state.isGeneratorStarted) {
+      stopGridGeneration();
+    } else {
+      startGridGeneration();
     }
-
-    return () => {
-      if (generationIntervalRef.current) {
-        clearInterval(generationIntervalRef.current)
-        generationIntervalRef.current = null
-      }
-    }
-
-  }, [isGeneratorStarted, isBiasInputDisabled, biasChar])
-
-  useEffect(() => {
-    if (lastBiasTime) {
-      const timeout = setTimeout(() => {
-        setIsBiasInputDisabled(false)
-      }, BIAS_COOLDOWN_MS);
-
-      return () => {
-        clearTimeout(timeout)
-      };
-    }
-  }, [lastBiasTime]);
-
-  function handleGenerateGrid() {
-    setIsGeneratorStarted(s => !s)
-  }
-
-  async function fetchGridData() {
-    try {
-      const { grid, lastBiasTime, prevBiasChar } = await generateGrid({ bias: biasChar });
-      const { code } = await getGridCode()
-      if (biasChar !== prevBiasChar && !isBiasInputDisabled && lastBiasTime + BIAS_COOLDOWN_MS > Date.now()) {
-        setLastBiasTime(lastBiasTime);
-        setIsBiasInputDisabled(true);
-      }
-
-      setGridData({ grid, code })
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  function getGridStatus() {
-    if (!isConnected) {
-      return {
-        status: "DISCONNECTED",
-        indicatorType: "disconnected",
-        message: "VERIFY IF BACKEND IS UP AND RUNNING",
-      };
-    }
-
-    if (isGeneratorStarted && gridData?.code) {
-      return {
-        status: "LIVE",
-        indicatorType: "live",
-        message: `YOUR CODE: ${gridData.code}`
-
-      };
-    }
-
-    return {
-      status: "READY",
-      indicatorType: "ready",
-      message: "READY FOR GRID GENERATION",
-    };
-  }
-
-  const { status, indicatorType, message } = getGridStatus();
+  };
 
   return (
     <div>
-      <div className='action-bar'>
+      <div className="action-bar">
         <Input
-          disabled={isBiasInputDisabled}
-          placeholder='Character'
-          label='Character'
-          onChange={onHandleBiasChar}
-          value={biasChar} />
+          disabled={state.isBiasInputDisabled}
+          placeholder="Character"
+          label="Character"
+          onChange={handleBiasCharChange}
+          value={state.biasChar}
+        />
         <ClockComponent />
-        <Button label={isGeneratorStarted ? 'HALT GENERATION' : 'GENERATE 2D GRID'} onClick={handleGenerateGrid} />
+        <Button
+          label={state.isGeneratorStarted ? 'HALT GENERATION' : 'GENERATE 2D GRID'}
+          onClick={handleGenerateGrid}
+        />
       </div>
-      <Grid data={gridData?.grid} />
-      <div className="grid-status-container">
-        <div className="grid-status-indicator-container">
-          <div className={clsx('grid-status-indicator', indicatorType)}></div>
-          <span>{status}</span>
-        </div>
-        <div className="grid-status-code-container">
-          <span>{message}</span>
-        </div>
-      </div>
+      <Grid data={state.gridData?.grid} />
+      <GeneratorStatus
+        isConnected={state.isConnected}
+        isGeneratorStarted={state.isGeneratorStarted}
+        gridCode={state.gridData?.code || null}
+      />
     </div>
-
-  )
+  );
 }
-
-
