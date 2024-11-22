f95275a743b14d5e17a26bd7eff8251285c79bd0
rodpadev <47316946+RodPaDev@users.noreply.github.com>
2024-11-22 16:26:46 +0000

    use mock db & new api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,18 @@ import express from 'express';
 import cors from 'cors';
 
 import gridRoutes from "./routes/grid"
+import paymentRoutes from "./routes/payment"
+import initAltarioDB from './db';
 
 const app = express();
 const PORT = process.env.EXPRESS_PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(initAltarioDB);
+
 app.use('/grid', gridRoutes);
+app.use('/payment', paymentRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
